237a74bb222b5d984db3b770eb894b9a32c1ccda
Nishi Kalra <kalra@AJAY-Lenovo.attlocal.net>
2024-08-12 17:20:09 -0500

    Created scrips for login and signup pages.

--- src/main/java/com/demoblazestore/pages/Sign_up.java
+++ src/main/java/com/demoblazestore/pages/Sign_up.java
@@ -1,5 +1,48 @@
 package com.demoblazestore.pages;
 
-public class Sign_up {
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.demoblazestore.base.DriverScripts;
+
+public class Sign_up extends DriverScripts {
+
+//********************************************Signuppagelocators*******************
+
+	@FindBy(id = "signin2") private WebElement signupLink;
+    @FindBy(id = "sign-username") private WebElement userNameTextbox;
+	@FindBy(id = "sign-password") private WebElement userPasswordTextbox;
+	@FindBy(xpath="//button[text()='Sign up']")  private WebElement signUpButton;
+	
+	
+	public Sign_up() {
+		PageFactory.initElements(driver, this);
+	}
+
+
+
+	public void signupLink_click() {
+		signupLink.click();
+
+	}
+	
+	public void signupUserEntry() {
+		userNameTextbox.sendKeys("jen1000");
+	
+	}
+	
+	public void signupPasswordEntry() {
+		userPasswordTextbox.sendKeys("jkl122");
+	}
+	
+	public void signupButtonClick() {
+		signUpButton.click();
+	}
 }
--- src/test/java/com/demoblazestore/test/Login_TestPage.java
+++ src/test/java/com/demoblazestore/test/Login_TestPage.java
@@ -1,5 +1,18 @@
 package com.demoblazestore.test;
+import org.testng.annotations.Test;
 
-public class Login_TestPage {
+import com.demoblazestore.pages.Login;
+
+public class Login_TestPage extends a_BaseTestPage  {
+
+	@Test
+	public void testLoginclick() throws InterruptedException {
+		Login login = new Login();
+		login.loginLink_click();
+		Thread.sleep(10000);
+		login.userName_Entry();
+		login.passwordEntry();
+		login.loginButtonClick();
+	}
 
 }
