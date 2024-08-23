4f39d06ee4c998feff907154265d569632d32b6a
bosed <bosed@Bose_ThinkPad.attlocal.net>
2024-08-23 09:48:33 -0700

    Adding test cases for homepage

--- src/main/java/com/demoblazestore/pages/Home.java
+++ src/main/java/com/demoblazestore/pages/Home.java
@@ -1,5 +1,139 @@
 package com.demoblazestore.pages;
 
-public class Home {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.demoblazestore.base.DriverScripts;
+
+public class Home extends DriverScripts {
+
+	//******HomePageLocators************
+
+		@FindBy(xpath = "//a[text()='Home ']") private WebElement homeButton;
+		@FindBy(xpath = "//a[text()='Contact']") private WebElement contactButton;
+		@FindBy(xpath = "//a[text()='About us']") private WebElement aboutusButton;
+		@FindBy(xpath = "//a[text()='Cart']") private WebElement cartButton;
+		@FindBy(xpath = "//a[text()='Log in']") private WebElement loginButton;
+		@FindBy(xpath = "//a[text()='Sign up']") private WebElement signupButton;
+		@FindBy(xpath = "//a[text()='Phones']") private WebElement phonesTab;
+		@FindBy(xpath = "//a[text()='Laptops']") private WebElement laptopsTab;
+		@FindBy(xpath = "//a[text()='Monitors']") private WebElement monitorsTab;
+		
+	//******************** Page Initialization***************************
+			public Home() {
+				PageFactory.initElements(driver, this);
+			}
+				
+	//******************** Page Methods/Actions***************************
+				
+		public String getHomePageTitle() {
+			return driver.getTitle();
+		}
+		
+		public void homeButton_click() {
+			homeButton.click();
+		}
+		
+		public String getHomeText() {
+			return homeButton.getText();
+		}
+		
+		public void contactButton_click() {
+			contactButton.click();
+		}
+		
+		public String getcontactButtonText() {
+			return contactButton.getText();
+		}
+		
+		public void aboutusButton_click() {
+			aboutusButton.click();
+		}
+		
+		public String getaboutusButtonText() {
+			return aboutusButton.getText();
+		}
+		
+		public void cartButton_click() {
+			cartButton.click();
+		}
+		
+		public String getcartButtonText() {
+			return cartButton.getText();
+		}
+		
+		public void loginButton_click() {
+			loginButton.click();
+		}
+		
+		public String getloginButtonText() {
+			return loginButton.getText();
+		}
+		
+		public void signupButton_click() {
+			signupButton.click();
+		}
+		
+		public String getsignupButtonText() {
+			return signupButton.getText();
+		}
+		
+		public boolean isHomeButtonPresent() {
+			return homeButton.isDisplayed();
+		}
+		
+		public boolean iscontactButtonPresent() {
+			return contactButton.isDisplayed();
+		}
+		
+		public boolean isaboutusButtonPresent() {
+			return aboutusButton.isDisplayed();
+		}
+		
+		public boolean iscartButtonPresent() {
+			return cartButton.isDisplayed();
+		}
+		
+				
+		public boolean isloginButtonPresent() {
+			return loginButton.isDisplayed();
+		}
+		
+		public boolean issignupButtonPresent() {
+			return signupButton.isDisplayed();
+		}
+		public void phonesTab_click() {
+			phonesTab.click();
+		}
+		
+		public String getphonesTabText() {
+			return phonesTab.getText();
+		}
+		
+		public boolean isphonesTabPresent() {
+			return phonesTab.isDisplayed();
+		}
+		public void laptopsTab_click() {
+			laptopsTab.click();
+		}
+		
+		public String getlaptopsTabText() {
+			return laptopsTab.getText();
+		}
+		
+		public boolean islaptopsTabPresent() {
+			return laptopsTab.isDisplayed();
+		}
+		public void monitorsTab_click() {
+			monitorsTab.click();
+		}
+		
+		public String getmonitorsTabText() {
+			return monitorsTab.getText();
+		}
+		
+		public boolean ismonitorsTabPresent() {
+			return monitorsTab.isDisplayed();
+		}
 }
--- src/test/java/com/demoblazestore/test/Home_TestPage.java
+++ src/test/java/com/demoblazestore/test/Home_TestPage.java
@@ -1,5 +1,96 @@
 package com.demoblazestore.test;
 
-public class Home_TestPage {
+import static org.testng.Assert.assertTrue;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.demoblazestore.pages.Home;
+
+public class Home_TestPage extends a_BaseTestPage {
+
+	@Test(priority = 1)
+	public void testHomeclick() throws InterruptedException {
+		// logger = report.createTest("test Home page click");
+		Home home = new Home();
+		String text = home.getHomeText();
+		System.out.println(text);
+		Assert.assertTrue(true, text);
+		Thread.sleep(3000);
+		// logger.pass("Home icon text matched");
+	}
+
+	@Test(priority = 2)
+	public void testContactclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getcontactButtonText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 3)
+	public void testAboutusclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getaboutusButtonText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 4)
+	public void testCartclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getcartButtonText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 5)
+	public void testLoginclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getloginButtonText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 6)
+	public void testSignupclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getsignupButtonText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 7)
+	public void testphonesTabclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getphonesTabText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 8)
+	public void testlaptopsTabclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getlaptopsTabText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 9)
+	public void testmonitorsTabclick() throws InterruptedException {
+		Home home = new Home();
+		String text = home.getmonitorsTabText();
+		System.out.println(text);
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 10)
+	public void testHomeLink() throws InterruptedException {
+		Home home = new Home();
+		home.monitorsTab_click();
+		Thread.sleep(3000);
+		String pageTitle = home.getHomePageTitle();
+		System.out.println(pageTitle);
+		Assert.assertTrue(true, pageTitle);
+	}
 }
