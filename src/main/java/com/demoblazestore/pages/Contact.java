af9671513fa47773d89ae2fb640b07422857585d
bosed <bosed@Bose_ThinkPad.attlocal.net>
2024-08-28 15:28:13 -0700

    Added test scripts for home page and contact page.

--- src/main/java/com/demoblazestore/pages/Contact.java
+++ src/main/java/com/demoblazestore/pages/Contact.java
@@ -1,5 +1,49 @@
 package com.demoblazestore.pages;
 
-public class Contact {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.demoblazestore.base.DriverScripts;
+
+public class Contact extends DriverScripts {
+	
+	//******ContactPageLocators************
+
+			@FindBy(id = "recipient-email") private WebElement ContactEmailTextBox;
+			@FindBy(id = "recipient-name") private WebElement ContactNameTextBox;
+			@FindBy(id = "message-text") private WebElement MessageTextBox;
+			@FindBy(xpath = "//*[@id=\"exampleModal\"]//button[@class='btn btn-secondary']") private WebElement CloseButton;
+			@FindBy(xpath = "//button[text()='Send message']") private WebElement SendMessageButton;
+
+	//******************** Page Initialization***************************
+			public Contact() {
+				PageFactory.initElements(driver, this);
+			}
+				
+	//******************** Page Methods/Actions***************************
+				
+		public String getContactPageTitle() {
+			return driver.getTitle();
+		}
+		
+		public void ContactEmailTextBox_sendText() {
+			ContactEmailTextBox.sendKeys("abc@abc.com");
+		}
+		
+		public void ContactNameTextBox_sendText() {
+			ContactNameTextBox.sendKeys("Jhon");
+		}
+		
+		public void MessageTextBox_sendText() {
+			MessageTextBox.sendKeys("Test Message for testing");
+		}
+		
+		public void CloseButton_click() {
+			CloseButton.click();
+		}
+		
+		public void SendMessageButton_click() {
+			SendMessageButton.click();
+		}
 }
--- src/test/java/com/demoblazestore/test/Contact_TestPage.java
+++ src/test/java/com/demoblazestore/test/Contact_TestPage.java
@@ -1,5 +1,34 @@
 package com.demoblazestore.test;
 
-public class Contact_TestPage {
+import org.testng.annotations.Test;
+import com.demoblazestore.pages.Contact;
+import com.demoblazestore.pages.Home;
 
+public class Contact_TestPage extends a_BaseTestPage {
+
+
+	@Test(priority = 1)
+	public void testSendMessage() throws InterruptedException {
+		Home home = new Home();
+		home.contactButton_click();
+		Contact contact = new Contact();
+		contact.ContactEmailTextBox_sendText();
+		contact.ContactNameTextBox_sendText();
+		contact.MessageTextBox_sendText();
+		Thread.sleep(3000);
+		contact.SendMessageButton_click();
+		
+}
+	@Test(priority = 2)
+	public void testCloseWithoutSendMessage() throws InterruptedException {
+		Home home = new Home();
+		home.contactButton_click();
+		Contact contact = new Contact();
+		contact.ContactEmailTextBox_sendText();
+		contact.ContactNameTextBox_sendText();
+		contact.MessageTextBox_sendText();
+		Thread.sleep(3000);
+		contact.CloseButton_click();
+		
+}
 }
