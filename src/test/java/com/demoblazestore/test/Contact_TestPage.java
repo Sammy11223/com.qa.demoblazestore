99d4670f91e0442565fd3d1ebbc44f8ffd07510e
sammy <43485170+Sammy11223@users.noreply.github.com>
2024-08-29 08:02:54 -0700

    Update Contact_TestPage.java
    Added some comments

--- src/test/java/com/demoblazestore/test/Contact_TestPage.java
+++ src/test/java/com/demoblazestore/test/Contact_TestPage.java
@@ -10,8 +10,8 @@ public class Contact_TestPage extends a_BaseTestPage {
 	@Test(priority = 1)
 	public void testSendMessage() throws InterruptedException {
 		homepage.contactButton_click();
-		Contact contact = new Contact();
-		contact.ContactEmailTextBox_sendText();
+		Contact contact = new Contact(); // Remove this line or comment it out
+		contact.ContactEmailTextBox_sendText(); // anywhere contact in this page should be replaced with contactpage , as this has been initialized already inside the a_BaseTestPage
 		contact.ContactNameTextBox_sendText();
 		contact.MessageTextBox_sendText();
 		Thread.sleep(3000);
@@ -21,8 +21,8 @@ public class Contact_TestPage extends a_BaseTestPage {
 	@Test(priority = 2)
 	public void testCloseWithoutSendMessage() throws InterruptedException {
 		homepage.contactButton_click();
-		Contact contact = new Contact();
-		contact.ContactEmailTextBox_sendText();
+		Contact contact = new Contact(); // Remove this line or comment it out
+		contact.ContactEmailTextBox_sendText(); // anywhere contact in this page should be replaced with contactpage , as this has been initialized already inside the a_BaseTestPage
 		contact.ContactNameTextBox_sendText();
 		contact.MessageTextBox_sendText();
 		Thread.sleep(3000);
